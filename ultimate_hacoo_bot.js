745ef5532108ae99639d9639730cfc69b50a51a6
Erdezz <erdezzroblox@gmail.com>
2026-03-28 23:35:22 +0100

    Update ultimate_hacoo_bot.js

--- ultimate_hacoo_bot.js
+++ ultimate_hacoo_bot.js
@@ -20,6 +20,8 @@ const TELEGRAM_CHANNELS = [
   "linkscrewfinds",
   "mkfashionfinds",
   "hacoolinksvip",
+  "HacooFranceLiens",
+  "hacoolinks10chanel",
 ];
 
 const LOCAL_CACHE_FILE = path.join(__dirname, "sent_links_cache.json");
